40639350008abceb5cc6552bd9f8d7eccb1c361c
Sumit Kosta <sumit.kosta1@trivialworks.com>
2022-08-29 14:41:51 +0530

    test changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.status(200).json({
         status: true,
-        message: 'Gojmgjhod Request'
+        message: 'Bad Request'
     })
 })
 
